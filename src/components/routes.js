31eb03e378a50b17280146a6b3ce1457584050a9
Kansei Kogami <kk7221dev@gmail.com>
2019-03-02 16:40:31 +0900

    create base dir-tree

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './main/Main'
 import Photo from './photo/Photo'
 import Product from './product/Product'
 
-class Routes extends React.Component {
+class Routes extends Component {
   render() {
     return(
       <BrowserRouter>
-        <Router exact path='/' component={Main} />
-        <Router exact path='photo' component={Photo}/>
-        <Router exact path='product' component={Product}/>
+        <div>
+          <Route exact path='/' component={Main} />
+          <Route exact path='/photo' component={Photo}/>
+          <Route exact path='/product' component={Product}/>
+        </div>
       </BrowserRouter>
     );
   }
 }
+
+export default Routes;
