f94f1340aba485ca36e6f8706f2cc02860d08323
Kansei Kogami <kk7221dev@gmail.com>
2019-03-03 02:33:10 +0900

    create header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import '../css/Header.css'
 
 class Header extends Component {
   render(){
     return(
-      <div>
-        Header
+      <div className="header">
+        <div className="logo"><img src="images/kk-logo-black.png" alt="kk"/></div>
+        <div className="menu">About me</div>
+        <div className="menu">Carrer</div>
+        <div className="menu">Product</div>
+        <div className="menu">Photo</div>
+        <div className="menu">Contact</div>
       </div>
     );
   }
