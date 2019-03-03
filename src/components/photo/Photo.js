ed880597a3888a14db523494c09b81bffc8cc78a
Kansei Kogami <kk7221dev@gmail.com>
2019-03-03 22:10:33 +0900

    add header to Photo

--- src/components/photo/Photo.js
+++ src/components/photo/Photo.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-
+import Header from '../Header'
 class Photo extends Component {
   render(){
     return(
       <div>
+        <Header isDefault={true} />
         Photo
       </div>
     );
