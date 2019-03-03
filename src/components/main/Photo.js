12f74708768e854ea1cd188dabc85a14aeb525ad
Kansei Kogami <kk7221dev@gmail.com>
2019-03-04 08:51:11 +0900

    fix photo, photograph

--- src/components/main/Photo.js
+++ src/components/main/Photo.js
@@ -7,10 +7,10 @@ const Photo = () => {
   const photo = (
     <div className="photo">
       <div className="photo-text">
-         　写真が好きです。日常にあるちぐはぐなモノを見つけるのが得意です。主にモノクロでスナップ写真を撮ります。最近は姪っ子と甥っ子を撮っています。
+         　写真が好きです。日常にあるちぐはぐなモノを見つけて撮っています。最近は姪っ子と甥っ子を撮っています。
       </div>
       <div className="photo-more">
-        <Link to="/photo"><div className="more-button">MORE</div></Link>
+        <Link to="/photograph"><div className="more-button">MORE</div></Link>
       </div>
     </div>
   );
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './main/Main'
-import Photo from './photo/Photo'
+import Photograph from './photograph/Photograph'
 import Product from './product/Product'
 
 class Routes extends Component {
@@ -10,7 +10,7 @@ class Routes extends Component {
       <BrowserRouter>
         <div>
           <Route exact path='/' component={Main} />
-          <Route exact path='/photo' component={Photo}/>
+          <Route exact path='/photograph' component={Photograph}/>
           <Route exact path='/product' component={Product}/>
         </div>
       </BrowserRouter>
