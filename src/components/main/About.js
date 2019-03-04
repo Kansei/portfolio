bcc085692c47423b75f26fbad23a339f2db88e4d
Kansei Kogami <kk7221dev@gmail.com>
2019-03-04 12:21:56 +0900

    fix about

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -9,12 +9,25 @@ const About = (props) => {
         <img src="/images/me.jpg" alt="me" />
       </div>
       <div className="introduction">
-        <div className="name">小神 寛晴(22)</div>
+        <div className="info">
+          <div className="name">小神 寛晴(22)</div>
+          <div className="affiliation">九州工業大学生命情報工学部4年</div>
+        </div>
         <div className="text">
-          九州工業大学生命情報工学部4年<br />
-          学部3年時に大学で得られた技術がないことに気づき、技術習得のため開発を始める。<br />
-          主にRails/sinatraを用いて、サーバーサイドの開発を行っている。<br />
-          大学では、遺伝子のメチル化からヒトの睡眠状態を予測する研究に従事。
+          <ul>
+            <li>
+            学部3年時に、これでまでの大学生活で身についた技術が一つもない事に気づき、何か技術を身に付けたいという漠然とした思いからアプリ開発を始める。
+            </li>
+            <li>
+            主に Rails/sinatra を用いてweb開発を行っており、現在はフロント開発に興味を持ち React を勉強中。
+            </li>
+            <li>
+            大学では、遺伝子のメチル化からヒトの睡眠状態を予測する研究に従事。
+            </li>
+            <li>
+            好きな食べ物は親子丼、唐揚げ、卵焼き
+            </li>
+          </ul>
          </div>
       </div>
     </div>
